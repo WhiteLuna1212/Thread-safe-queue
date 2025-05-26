adb8f2985fff1d431a6a19d6c08af2b517ce75cc
WhiteLuna1212 <whitelunafoxstar@gmail.com>
2025-05-26 10:15:56 +0900

    Create queue.cpp
    초기 queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,25 @@
+//큐 구현 (제출)
+
+#include <iostream>
+#include "queue.h"
+
+Queue* init(void) {
+	return NULL;
+}
+
+void release(Queue* queue) {
+	return;
+}
+
+Node* nalloc(Item item) {
+	return NULL;
+}
+
+void nfree(Node* node) {
+	return;
+}
+
+Node* nclone(Node* node) {
+	return NULL;
+}
+
