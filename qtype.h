c119ae8877b91e55cb407901316e230a956639c3
WhiteLuna1212 <whitelunafoxstar@gmail.com>
2025-05-26 10:15:18 +0900

    Create qtype.h
    초기 qtype.h 파일

---
+++ qtype.h
@@ -0,0 +1,28 @@
+//수정 가능(제출O)
+#pragma once
+#include <stdbool.h> 
+
+typedef int Key;      
+typedef int Value;     
+
+typedef struct {
+	Key key;
+	Value value;
+} Item;
+
+typedef struct {
+	bool success;   // true: 성공, false: 실패
+	Item item;
+	// 필드추가가능
+} Reply;
+
+typedef struct node_t {
+	Item item;
+	struct node_t* next;
+	// 필드추가가능
+} Node;
+
+typedef struct {
+	Node* head, tail;
+	// 필드추가가능
+} Queue;
